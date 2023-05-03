be3a28d965f734ef954ba1563ae1c178d6958750
Rifqi Faisal <129919617+Rifqi092@users.noreply.github.com>
2023-05-03 12:39:53 +0800

    Operator Binary Aritmatika

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,15 @@
+package operator;
+
+public class OperatorBinary {
+    public static void main(String[] args) {
+        String nama = "Muhammad Rifqi Faisal";
+        int jumlahSks = 24;
+        double indeksPrestasiKumulatif = 3.89;
+        System.out.println(9+3);
+        System.out.println(9-3);
+        System.out.println(9*3);
+        System.out.println(9/3);
+        System.out.println(9%2);
+        System.out.println(nama+" Jumlah SKS yang diambil: "+jumlahSks+" dengan nilai= "+indeksPrestasiKumulatif);
+    }
+}
